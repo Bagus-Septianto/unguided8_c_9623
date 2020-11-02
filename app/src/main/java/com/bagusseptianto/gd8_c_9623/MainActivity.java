b34fe21794c23feced85843a1c2c960f481ec001
Bagus-Septianto <180709623@students.uajy.ac.id>
2020-11-02 17:11:40 +0700

    master file

---
+++ app/src/main/java/com/bagusseptianto/gd8_c_9623/MainActivity.java
@@ -0,0 +1,37 @@
+package com.bagusseptianto.gd8_c_9623;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+    private CardView cvCreateUser, cvShowListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cvCreateUser = findViewById(R.id.cvCreateUser);
+        cvShowListener = findViewById(R.id.cvShowListUser);
+
+        cvCreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CreateUserActivity.class);
+                startActivity(i);
+            }
+        });
+
+        cvShowListener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ShowListUserActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/bagusseptianto/gd8_c_9623/UserResponse.java
@@ -0,0 +1,32 @@
+package com.bagusseptianto.gd8_c_9623;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class UserResponse {
+    @SerializedName("data")
+    @Expose
+    private List<UserDAO> users = null;
+
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    public List<UserDAO> getUsers() {
+        return users;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setUsers(List<UserDAO> users) {
+        this.users = users;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
